dc18af76e238833fc3d61ccd1d136e6c1860ff24
yanglingzi <15592276397@163.com>
2018-10-12 21:59:18 +0800

    这是第二个版本

--- js/index.js
+++ js/index.js
@@ -1,33 +1,13 @@
 
 $(function(){
-	$(".fw").css({
-		width:"76px",
-		position:"absolute",
-		top:"34px",
-		left:0,
-		border:"1px solid #737373",
-		
-	});
-	$(".kehu").mouseenter(function(){
-		$(this).css({
-			background:"#fff",
-			border:"1px solid #d9d9d9",
-		});
-		$(".kehu .sanjiao").css("border-top-color","#ff6a00");
-	});
-	
-	$(".kehu").mouseleave(function(){
-		$(this).css({
-			background:"#f2f2f2",
-			border:"1px solid #f2f2f2",
-			padding:"0 15px 0 10px"
-		});
-		$(".kehu .sanjiao").css("border-top-color","#d8d8d8");
-	});
-	
-	$(".gouwu").mouseenter(function(){
-		$(this).css({
-			background:"#fff",
-		})
-	});
-});
+ 	//轮播图上右边的图片
+    $("#banner .rightbox img").hover(function(){
+      $(this).animate({
+        'left' : '-5px'
+      },'normal');
+    },function(){
+      $(this).animate({
+        'left' : '0px'
+      },'normal');      
+    });
+  });
